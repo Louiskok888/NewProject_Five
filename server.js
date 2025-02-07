dcd6a1120a62c546282cd31e4df37069828f3530
Louiskok <bluecrab_888@hotmail.com>
2025-02-07 13:59:05 +0800

    new10

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // Connect to MongoDB Atlas
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to MongoDB Atlas"))
     .catch(err => console.error("Error connecting to MongoDB", err));
 
